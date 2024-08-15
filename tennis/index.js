af254094498a678e47c0cc58f9b411cad527ddd7
Edward <edwarddac.56@gmail.com>
2024-08-15 18:45:00 -0500

    add function cropper

--- tennis/index.js
+++ tennis/index.js
@@ -1,1 +1,81 @@
-console.log('gaaa')
+(function () {
+	
+    let inputFile = document.getElementById("imageUpload"),
+		popupCropper = document.querySelector(".popup_cropper"),
+		popupCropperCanvas = document.querySelector(".popup_cropper-canvas");
+
+    let image4Photo = document.querySelector('#imageContainer img');
+
+    let cropper;
+
+	if (inputFile) {
+		inputFile.addEventListener("change", function (e) {
+			document.body.classList.add("loading");
+
+			var file = e.target.files[0];
+			var reader = new FileReader();
+
+            image4Photo.src = '';
+            document.querySelector('.generate_image').disabled = true;
+
+            setTimeout(() => {
+
+                reader.onload = function (event) {
+                    // Crear un nuevo elemento img
+                    let img = document.createElement("img");
+                    img.id = 'croppy';
+                    img.src = event.target.result; // Usar la URL de la imagen cargada
+    
+                    // Limpiar cualquier imagen previa en el canvas
+                    popupCropperCanvas.innerHTML = "";
+                    popupCropperCanvas.appendChild(img);
+    
+                    // Quitar el loading y mostrar el popup
+                    document.body.classList.remove("loading");
+
+                    popupCropper.classList.add("active");
+
+
+                    cropper = new Cropper(img, {
+                        aspectRatio: 16/10
+                    });
+
+                };
+    
+                // Leer el archivo como una URL
+                reader.readAsDataURL(file);
+                
+            }, 1000);
+
+
+            popupCropper.querySelector('#cut').addEventListener('click', (e)=>{
+                e.preventDefault();
+
+                var croppedImage = cropper.getCroppedCanvas().toDataURL("image/png");
+                image4Photo.src = croppedImage;
+                document.querySelector('.generate_image').disabled = false;
+
+                resetCropper();
+            })
+
+            popupCropper.querySelector('#clean').addEventListener('click', (e)=>{
+                e.preventDefault();
+                resetCropper();
+            });
+
+		});
+
+
+        function resetCropper(){
+            if(popupCropper.classList.contains("active")){
+                popupCropper.classList.remove("active");
+            }
+            document.getElementById("imageUpload").value = '';
+            setTimeout(() => {
+                popupCropperCanvas.innerHTML = '';
+            }, 500);
+        }
+
+	}
+
+})();
--- tennis/index.php
+++ tennis/index.php
@@ -7,10 +7,34 @@
     <title>Copa Level Up 2024</title>
     <link rel="stylesheet" href="css/style.css">
     <!-- <script defer src="https://cdn.jsdelivr.net/npm/@vladmandic/face-api@latest/dist/face-api.min.js"></script> -->
-    <script defer src="index.js"></script>
+    <link rel="stylesheet" href="cropper/cropper.css">
+    <script src="cropper/cropper.js"></script>
 </head>
 
 <body>
+
+    <div class="popup_cropper">
+        <div class="popup_cropper-bg"></div>
+        <div class="popup_cropper-box">
+            <div class="popup_cropper-canvas">
+                <!-- cropper -->
+            </div>
+            <div class="popup_cropper-actions">
+                <button type="button" id="clean">
+                    Limpiar
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z" />
+                    </svg>
+                </button>
+                <button type="button" id="cut">
+                    Recortar
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M448 109.3l54.6-54.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L402.7 64 160 64l0 64 178.7 0L128 338.7 128 32c0-17.7-14.3-32-32-32S64 14.3 64 32l0 32L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l32 0 0 256c0 35.3 28.7 64 64 64l224 0 0-64-178.7 0L384 173.3 384 480c0 17.7 14.3 32 32 32s32-14.3 32-32l0-32 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-32 0 0-274.7z" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
 
     <div class="loader_fixed">
         <span class="loader"></span>
@@ -187,7 +211,9 @@
                         <p><?php echo htmlspecialchars($datos['titulo'] ?? ''); ?></p>
                         <h1>Resultados del partido</h1>
                     </div>
-                    <div class="canvas_image" id="imageContainer"></div>
+                    <div class="canvas_image" id="imageContainer">
+                        <img src="" title="Foto" alt="Foto">
+                    </div>
                 </div>
                 <div class="canvas_scores">
                     <div class="canvas_scores-top canvas_scores-points">
@@ -211,7 +237,7 @@
                     <div class="canvas_scores-bottom canvas_scores-points">
                         <div class="masked">
                             <?php if (isset($datos['jugador_ganador_dos']) && !empty($datos['jugador_ganador_dos'])): ?>
-                                <p class="less"><?php echo $datos['jugador_rival_uno'].' + '.$datos['jugador_rival_dos']; ?></p>
+                                <p class="less"><?php echo $datos['jugador_rival_uno'] . ' + ' . $datos['jugador_rival_dos']; ?></p>
                             <?php else: ?>
                                 <p><?php echo $datos['jugador_rival_uno'] ?? ''; ?></p>
                             <?php endif; ?>
@@ -250,6 +276,7 @@
         ?>
 
         <script src="js/html2canvas.min.js"></script>
+        <script src="index.js"></script>
         <script>
             document.addEventListener('DOMContentLoaded', (event) => {
 
@@ -322,6 +349,11 @@
                     applyClipPathToCanvas(newCanvas, clipPaths);
 
                     let dataURL = newCanvas.toDataURL('image/png');
+
+                    if (document.body.classList.contains("loading")) {
+                        document.body.classList.remove("loading")
+                    }
+
                     downloadImage(dataURL, filename);
                 }
 
@@ -329,9 +361,8 @@
                     generateButton.addEventListener('click', (e) => {
                         e.preventDefault();
 
-                        if (inputImage && inputImage.files.length == 0) {
-                            alert('Asegúrese de haber cargado una imagen');
-                            return;
+                        if (!document.body.classList.contains("loading")) {
+                            document.body.classList.add("loading")
                         }
 
                         setTimeout(() => {
